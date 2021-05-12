180b2c5a9766d19e5a622eb3377a1d005daadbf2
Jason Bailey <jmbailey816@gmail.com>
2021-05-12 16:34:10 -0400

    Code cleanup

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var symbols = "!@#$%^&*_-+=";
 
 // declare variables for each of the elements - 
 var passwordTxt = document.getElementById("password");
-/* const incAlphalower = document.getElementById("alphalower"); use this if you figure out how to have lowercase as an option */
+var incAlphalower = document.getElementById("alphalower"); 
 var incAlphaupper = document.getElementById("alphaupper");
 var incNumbers = document.getElementById("numbers");
 var incSymbols = document.getElementById("symbols");
@@ -15,34 +15,49 @@ var generateBtn = document.getElementById("generate");
 
 // when generate password button clicked, check if numbers and symbols boxes are checked, generate password based on length and variables selected
 generateBtn.addEventListener("click", () => {
-  let characters = alphalower;
-  /* incAlphalower.checked ? (characters += alphalower) : ""; */
+
+  let characters = "";
+
+  incAlphalower.checked ? (characters += alphalower) : "";
+
   incAlphaupper.checked ? (characters += alphaupper) : ""; //The addition assignment operator (+=) adds the value of the right operand to a variable and assigns the result to the variable. The types of the two operands determine the behavior of the addition assignment operator. Addition or concatenation is possible.
 
   incNumbers.checked ? (characters += numbers) : ""; // ".checked ?" determines if the option is selected then it will include var numbers 
+
   incSymbols.checked ? (characters += symbols) : ""; // what do the colon and quote marks do?
+
   passwordTxt.value = generatePassword(length.value, characters); //what does passwordTxt.value do here?
 });
 
-// create password
-// I don't really understand any of this
+// create random password, first by saying the amount of characters (length) and the selected arrays (characters)
+
+
 var generatePassword = (length, characters) => {
   let password = "";
   for (let i = 0; i < length; i++) {
-    password += characters.charAt(
-      Math.floor(Math.random() * characters.length)
+    password += characters.charAt( // charAt = character At -- essentially what position a character is in the array 
+      Math.floor(Math.random() * characters.length) // this randomly picks a character out from the array until the length has been met
     );
   }
   return password;
 };
 
+
+
 // copy to clipboard
 var copyBtn = document.getElementById("copy"); //gets from the html the id copy
+
 copyBtn.addEventListener("click", () => { //when the button is clicked do something 
-  passwordTxt.select(); // no idea 
+
+  passwordTxt.select(); // this is defined earlier by id #password as the text box, so select the contents of this text box 
+
   document.execCommand("copy"); //copies to clipboard?
   alert("Password Copied"); //creates popup at top of screen with message
 });
+
+
+
+
 
 //slider
 function updateTextInput(val) { // no idea 
